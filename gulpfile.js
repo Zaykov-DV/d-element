9bffedc062f1ea3e95da491eb4d0b19b972557c6
Zaykov-DV <65911658+Zaykov-DV@users.noreply.github.com>
2021-07-01 22:55:19 +0500

    Верстка

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const server = require('browser-sync')
+
+gulp.task('css', function () {
+ return gulp.src('scss/main.scss')
+     .pipe(sass())
+     .pipe(gulp.dest('css'))
+})
+
+gulp.task('server', function () {
+      server.init({
+       server: '.',
+       notify: false
+      })
+
+gulp.watch(('scss/common/*.scss' || 'scss/*.scss') , gulp.series('css')).on('change', server.reload)
+})
